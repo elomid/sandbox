2237c81fe32fe5259e8dab62f4fc337e806811d9
Omid <omid.elli@gmail.com>
2021-03-26 09:07:52 -0700

    tooltip logic inside each component - not ideal

--- src/App.js
+++ src/App.js
@@ -11,25 +11,51 @@ function Tooltip({ text, position }) {
   );
 }
 
-function Summary() {
-  return (
-    <div className="data-container">
-      <div className="data-label tooltip-container">
-        Monthly revenue
-        <Tooltip text="Summary tooltip" position="right" />
+class Summary extends React.Component {
+  state = {
+    hovering: false
+  };
+  mouseEnter = () => this.setState({ hovering: true });
+  mouseLeave = () => this.setState({ hovering: false });
+  render() {
+    return (
+      <div className="data-container">
+        <div
+          className="data-label tooltip-container"
+          onMouseEnter={this.mouseEnter}
+          onMouseLeave={this.mouseLeave}
+        >
+          Monthly revenue
+          {this.state.hovering ? (
+            <Tooltip text="Summary tooltip" position="right" />
+          ) : null}
+        </div>
+        <div className="data-value">$67,890.0</div>
       </div>
-      <div className="data-value">$67,890.0</div>
-    </div>
-  );
+    );
+  }
 }
 
-function Chart() {
-  return (
-    <div className="chart tooltip-container">
-      Chart goes here
-      <Tooltip text="Chart tooltip" position="bottom" />
-    </div>
-  );
+class Chart extends React.Component {
+  state = {
+    hovering: false
+  };
+  mouseEnter = () => this.setState({ hovering: true });
+  mouseLeave = () => this.setState({ hovering: false });
+  render() {
+    return (
+      <div
+        className="chart tooltip-container"
+        onMouseEnter={this.mouseEnter}
+        onMouseLeave={this.mouseLeave}
+      >
+        Chart goes here
+        {this.state.hovering ? (
+          <Tooltip text="Chart tooltip" position="bottom" />
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default function App() {
