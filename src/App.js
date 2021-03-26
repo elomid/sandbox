9ffc136174b28f7dfd121b48fca594f2c5fef77e
Omid <omid.elli@gmail.com>
2021-03-26 10:49:29 -0700

    HOC withHover Created a HOC that handles the hovering state and passes down true or false to the wrapped component as a prop. This means that you need to update the wrapped components to reflect the fact that they are receiving hovering as props.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import React from "react";
 import "./styles.css";
 
+function withHover(WrappedComponent) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        hovering: false
+      };
+      this.mouseEnter = this.mouseEnter.bind(this);
+      this.mouseLeave = this.mouseLeave.bind(this);
+    }
+    mouseEnter() {
+      this.setState({ hovering: true });
+    }
+    mouseLeave() {
+      this.setState({ hovering: false });
+    }
+    render() {
+      return (
+        <div onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
+          {<WrappedComponent hovering={this.state.hovering} {...this.props} />}
+        </div>
+      );
+    }
+  };
+}
+
 function Tooltip({ text, position }) {
   return (
     <div
@@ -11,58 +37,39 @@ function Tooltip({ text, position }) {
   );
 }
 
-class Summary extends React.Component {
-  state = {
-    hovering: false
-  };
-  mouseEnter = () => this.setState({ hovering: true });
-  mouseLeave = () => this.setState({ hovering: false });
-  render() {
-    return (
-      <div className="data-container">
-        <div
-          className="data-label tooltip-container"
-          onMouseEnter={this.mouseEnter}
-          onMouseLeave={this.mouseLeave}
-        >
-          Monthly revenue
-          {this.state.hovering ? (
-            <Tooltip text="Summary tooltip" position="right" />
-          ) : null}
-        </div>
-        <div className="data-value">$67,890.0</div>
-      </div>
-    );
-  }
-}
-
-class Chart extends React.Component {
-  state = {
-    hovering: false
-  };
-  mouseEnter = () => this.setState({ hovering: true });
-  mouseLeave = () => this.setState({ hovering: false });
-  render() {
-    return (
-      <div
-        className="chart tooltip-container"
-        onMouseEnter={this.mouseEnter}
-        onMouseLeave={this.mouseLeave}
-      >
-        Chart goes here
-        {this.state.hovering ? (
-          <Tooltip text="Chart tooltip" position="bottom" />
+function Summary(props) {
+  return (
+    <div className="data-container">
+      <div className="data-label tooltip-container">
+        Monthly revenue
+        {props.hovering ? (
+          <Tooltip text="Summary tooltip" position="right" />
         ) : null}
       </div>
-    );
-  }
+      <div className="data-value">$67,890.0</div>
+    </div>
+  );
 }
+
+function Chart(props) {
+  return (
+    <div className="chart tooltip-container">
+      Chart goes here
+      {props.hovering ? (
+        <Tooltip text="Chart tooltip" position="bottom" />
+      ) : null}
+    </div>
+  );
+}
+
+const SummaryWithHover = withHover(Summary);
+const ChartWithHover = withHover(Chart);
 
 export default function App() {
   return (
     <div className="App">
-      <Summary />
-      <Chart />
+      <SummaryWithHover />
+      <ChartWithHover />
     </div>
   );
 }
