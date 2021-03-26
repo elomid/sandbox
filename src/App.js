05f55d6ee5c94fdfaac95db2f20f907876c77527
Omid <omid.elli@gmail.com>
2021-03-26 09:02:23 -0700

    Tooltip skeleton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from "react";
 import "./styles.css";
 
+function Tooltip({ text, position }) {
+  return (
+    <div
+      className={position === "bottom" ? "tooltip" : "tooltip tooltip-right"}
+    >
+      {text}
+    </div>
+  );
+}
+
 function Summary() {
   return (
     <div className="data-container">
-      <div className="data-label">Monthly revenue</div>
+      <div className="data-label tooltip-container">
+        Monthly revenue
+        <Tooltip text="Summary tooltip" position="right" />
+      </div>
       <div className="data-value">$67,890.0</div>
     </div>
   );
 }
 
 function Chart() {
-  return <div className="chart">Chart goes here</div>;
+  return (
+    <div className="chart tooltip-container">
+      Chart goes here
+      <Tooltip text="Chart tooltip" position="bottom" />
+    </div>
+  );
 }
 
 export default function App() {
